4a6c53632ceae3513a0c5387a990e70bf2b623eb
Rimsha shahwar <rimshashahwar@gmail.com>
2026-04-14 16:53:06 +0530

    Added root route

--- backend/main.py
+++ backend/main.py
@@ -90,6 +90,12 @@ if not RAZORPAY_KEY_ID or not RAZORPAY_KEY_SECRET:
 
 razorpay_client = razorpay.Client(auth=(RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET))
 
+
+@app.get("/")
+def root():
+    return {"message": "E-commerce backend is running 🚀"}
+
+
 # =========================================================
 # OWNER
 # =========================================================
